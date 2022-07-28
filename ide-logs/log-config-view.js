3f4552db1e2d3359dd5b2ad486a612d57d0e5b0b
delchev <delchevn@gmail.com>
2022-07-28 20:22:40 +0300

    log-config l&f update

--- ide-logs/log-config-view.js
+++ ide-logs/log-config-view.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -8,13 +8,18 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-exports.getView = function () {
-	return {
-		"id": "logconfig",
-		"name": "LogConfig",
-		"factory": "frame",
-		"region": "center-bottom",
-		"label": "Log Config",
-		"link": "../ide-logs/log-config.html"
-	};
+
+const viewData = {
+	id: "logconfig",
+	name: "LogConfig",
+	factory: "frame",
+	region: "bottom",
+	label: "Log Config",
+	link: "../ide-logs/log-config.html"
+};
+
+if (typeof exports !== 'undefined') {
+	exports.getView = function () {
+		return viewData;
+	}
 }
---
+++ ide-logs/log-config.js
@@ -0,0 +1,34 @@
+const logconfigurationsView = angular.module('logconfigurations', ['ideUI', 'ideView', "ngRoute"]);
+
+logconfigurationsView.config(["messageHubProvider", function (messageHubProvider) {
+    messageHubProvider.eventIdPrefix = 'logconfigurations-view';
+}]);
+
+logconfigurationsView.controller('LogConfigurationsController', ['$scope', '$http', '$route', 'messageHub', function ($scope, $http, $route, messageHub) {
+
+    let logConfigurationsApi = '/services/v4/ops/logconfig';
+
+    function loadLogConfigurations() {
+        $http.get(logConfigurationsApi)
+            .then(function (data) {
+                $scope.logConfigurations = data.data;
+            });
+    }
+
+    $scope.setSeverity = function (loggerName, loggerLevel) {
+        $http.post(logConfigurationsApi + "/severity/" + loggerName, loggerLevel)
+            .then(function (data) {
+                let logger = (element) => element.name === loggerName;
+                let i = $scope.logConfigurations.findIndex(logger);
+                $scope.logConfigurations[i].severity = data.data;
+                $route.reload();
+            }), function (data) {
+                let logger = (element) => element.name === loggerName;
+                let i = $scope.logConfigurations.findIndex(logger);
+                $scope.logConfigurations[i].severity = data.data;
+                $route.reload()
+            }
+            ;
+    }
+    loadLogConfigurations();
+}]);
